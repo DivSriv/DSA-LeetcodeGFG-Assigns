5636513ea3c7bdc851f7b811daf20d911d8a22a0
Divya Srivastava <72649014+DivSriv@users.noreply.github.com>
2022-07-30 20:08:40 +0530

    Create SubsetSumProblem.js

---
+++ Problems/Solutions/GFG/SubsetSumProblem.js
@@ -0,0 +1,132 @@
+/*Subset Sum Problem(https://practice.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1)
+
+Given an array of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum. 
+
+
+Example 1:
+
+Input:
+N = 6
+arr[] = {3, 34, 4, 12, 5, 2}
+sum = 9
+Output: 1 
+Explanation: Here there exists a subset with
+sum = 9, 4+3+2 = 9.
+Example 2:
+
+Input:
+N = 6
+arr[] = {3, 34, 4, 12, 5, 2}
+sum = 30
+Output: 0 
+Explanation: There is no subset with sum 30.
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function isSubsetSum() which takes the array arr[], its size N and an integer sum as input parameters and returns boolean value true if there exists a subset with given sum and false otherwise.
+The driver code itself prints 1, if returned value is true and prints 0 if returned value is false.
+ 
+
+Expected Time Complexity: O(sum*N)
+Expected Auxiliary Space: O(sum*N)
+ 
+
+Constraints:
+1 <= N <= 100
+1<= arr[i] <= 100
+1<= sum <= 105
+*/
+
+
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @param {number} sum
+ * @return {boolean}
+ */
+
+class Solution {
+    isSubsetSum(arr,n,sum){
+        //code here
+        let dp = Array(arr.length).fill().map(()=>Array(sum+1).fill(-1))
+        function subSum(index,sum){
+            if(sum==0){return true}
+            
+            if(index==0){
+                if(arr[index]==sum){return true}
+                else{return false}
+            }
+            
+            if(dp[index][sum]!==-1){return dp[index][sum]}
+            
+            let prev = subSum(index-1,sum)
+            let curr = false
+            if(arr[index]<=sum){
+                curr = subSum(index-1,sum-arr[index])
+            }
+            return dp[index][sum]= prev || curr
+        }
+        return subSum(arr.length-1,sum)
+    }
+}
+
+//https://www.youtube.com/watch?v=_gPcYovP7wc&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=8
+
+//{ Driver Code Starts
+//Initial Template for javascript
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/* Function to print an array */
+function printArray(arr, size) {
+  let i;
+  let s = "";
+  for (i = 0; i < size; i++) {
+    s += arr[i] + " ";
+  }
+  console.log(s);
+}
+
+function main() {
+  let t = parseInt(readLine());
+  let i = 0;
+  for (; i < t; i++) {
+    let n = parseInt(readLine());
+    let arr = new Array(n);
+    let inputArr = readLine().split(" ").map((x) => parseInt(x));
+    for(let j = 0;j<n;j++) arr[j] = inputArr[j];
+    let sum = parseInt(readLine());
+    let obj = new Solution();
+    let res = obj.isSubsetSum(arr,n,sum);
+    console.log(Number(res));
+  }
+}
+// } Driver Code Ends

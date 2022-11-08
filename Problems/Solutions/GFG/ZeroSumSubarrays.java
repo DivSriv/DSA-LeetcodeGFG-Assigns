5ac8d22c08d0fc5a2148acb53bbb6832846971cf
Divya Srivastava <72649014+DivSriv@users.noreply.github.com>
2022-11-08 21:55:55 +0530

    Create ZeroSumSubarrays.java

---
+++ Problems/Solutions/GFG/ZeroSumSubarrays.java
@@ -0,0 +1,106 @@
+//Zero Sum Subarrays (https://practice.geeksforgeeks.org/problems/zero-sum-subarrays1825/1)
+
+/*
+
+You are given an array arr[] of size n. Find the total count of sub-arrays having their sum equal to 0.
+
+
+Example 1:
+
+Input:
+n = 6
+arr[] = {0,0,5,5,0,0}
+Output: 6
+Explanation: The 6 subarrays are 
+[0], [0], [0], [0], [0,0], and [0,0].
+
+
+Example 2:
+
+Input:
+n = 10
+arr[] = {6,-1,-3,4,-2,2,4,6,-12,-7}
+Output: 4
+Explanation: The 4 subarrays are [-1 -3 4]
+[-2 2], [2 4 6 -12], and [-1 -3 4 -2 2]
+
+Your Task:
+You don't need to read input or print anything. Complete the function findSubarray() that takes the array arr and its size n as input parameters and returns the total number of sub-arrays with 0 sum. 
+ 
+
+Expected Time Complexity : O(n)
+Expected Auxilliary Space : O(n)
+ 
+
+Constraints:    
+1 <= n <= 10^7
+-10^10 <= arri <= 10^10
+
+*/
+
+
+//User function Template for Java
+
+class Solution{
+    //Function to count subarrays with sum equal to 0.
+    public static long findSubarray(long[] arr ,int n) 
+    {
+        HashMap<Long,Long> map = new HashMap<>();
+        map.put(0L,1L);
+        long count=0;
+        long sum=0;
+        for(long val:arr){
+            sum+=val;
+            Long freq = map.get(sum);
+            if(freq != null){
+                count+=freq;
+                map.put(sum,freq+1);
+            }
+            else{
+                map.put(sum,1L);
+            }
+        }
+        return count;
+    }
+}
+
+
+
+
+
+
+
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		sc.nextLine();
+		while(t-->0)
+		{
+		    int n;
+		    n=sc.nextInt();
+		    
+		    long arr[]=new long[n];
+	
+		    
+		    for(int i=0;i<n;i++)
+		    {
+		        arr[i]=sc.nextLong();
+		    }
+            Solution ob = new Solution();
+		    System.out.println(ob.findSubarray(arr,n));
+		}
+		
+	}
+}
+
+// } Driver Code Ends
